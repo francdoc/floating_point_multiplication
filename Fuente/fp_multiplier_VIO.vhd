767e0f9f9df3668067270f93ec2308744e455755
FranCDoc <fchiesadoc@gmail.com>
2025-04-21 19:43:21 -0300

    Tratando de desarrollar VIO

---
+++ Fuente/fp_multiplier_VIO.vhd
@@ -0,0 +1,64 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+-- Wrapper VIO para el multiplicador de punto flotante fp_multiplier
+entity fp_multiplier_VIO is
+  port (
+    clk_i : in std_logic -- Señal de reloj (clock) que sincroniza la operación del multiplicador
+  );
+end fp_multiplier_VIO;
+
+architecture fp_multiplier_VIO_arch of fp_multiplier_VIO is
+
+  -- Declaración del componente fp_multiplier
+  component fp_multiplier is
+    port (
+      clk    : in  std_logic;
+      rst    : in  std_logic;
+      A      : in  std_logic_vector(31 downto 0);
+      B      : in  std_logic_vector(31 downto 0);
+      result : out std_logic_vector(31 downto 0)
+    );
+  end component;
+
+  -- Componente VIO generado por IP
+  COMPONENT vio_0 is
+    PORT (
+      clk        : in  STD_LOGIC;
+      probe_in0  : in  STD_LOGIC_VECTOR(31 DOWNTO 0); -- result
+      probe_out0 : out STD_LOGIC_VECTOR(31 DOWNTO 0); -- A
+      probe_out1 : out STD_LOGIC_VECTOR(31 DOWNTO 0); -- B
+      probe_out2 : out STD_LOGIC_VECTOR(0 DOWNTO 0)   -- rst
+    );
+  end component;
+
+  -- Señales de conexión entre VIO y fp_multiplier
+  signal vio_A   : std_logic_vector(31 downto 0);
+  signal vio_B   : std_logic_vector(31 downto 0);
+  signal vio_RST : std_logic_vector(0 downto 0);
+  signal int_res : std_logic_vector(31 downto 0);
+
+begin
+
+  -- Instancia del multiplicador de punto flotante
+  fp_multiplier_inst : fp_multiplier
+    port map(
+      clk    => clk_i,
+      rst    => vio_RST(0),
+      A      => vio_A,
+      B      => vio_B,
+      result => int_res
+    );
+
+  -- Instancia del bloque VIO para inyectar A, B, rst y monitorear result
+  vio_inst : vio_0
+    PORT MAP(
+      clk        => clk_i,
+      probe_in0  => int_res,
+      probe_out0 => vio_A,
+      probe_out1 => vio_B,
+      probe_out2 => vio_RST
+    );
+
+end fp_multiplier_VIO_arch;
---
+++ Sintesis/Sintesis.srcs/sources_1/ip/vio_0/vio_0_stub.v
@@ -0,0 +1,25 @@
+// Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2018.1 (lin64) Build 2188600 Wed Apr  4 18:39:19 MDT 2018
+// Date        : Mon Apr 21 01:26:28 2025
+// Host        : franco-ThinkPad-E15-Gen-2 running 64-bit Ubuntu 22.04.5 LTS
+// Command     : write_verilog -force -mode synth_stub
+//               /home/franco/CLP/Recursos/TP_FINAL/Sintesis/Sintesis.srcs/sources_1/ip/vio_0/vio_0_stub.v
+// Design      : vio_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z010clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* X_CORE_INFO = "vio,Vivado 2018.1" *)
+module vio_0(clk, probe_in0, probe_out0, probe_out1, 
+  probe_out2)
+/* synthesis syn_black_box black_box_pad_pin="clk,probe_in0[31:0],probe_out0[31:0],probe_out1[31:0],probe_out2[0:0]" */;
+  input clk;
+  input [31:0]probe_in0;
+  output [31:0]probe_out0;
+  output [31:0]probe_out1;
+  output [0:0]probe_out2;
+endmodule
