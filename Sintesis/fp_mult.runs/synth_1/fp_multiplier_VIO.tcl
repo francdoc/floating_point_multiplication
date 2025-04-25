aa59992a6863c104cce2c1fab129243bb572b284
FranCDoc <fchiesadoc@gmail.com>
2025-04-25 01:09:52 -0300

    Redoing bitstream and report

--- Sintesis/fp_mult.runs/synth_1/fp_multiplier_VIO.tcl
+++ Sintesis/fp_mult.runs/synth_1/fp_multiplier_VIO.tcl
@@ -16,7 +16,7 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
-set_param synth.incrementalSynthesisCache /tmp/.Xil_franco/Vivado-1837019-franco-ThinkPad-E15-Gen-2/incrSyn
+set_param synth.incrementalSynthesisCache /tmp/.Xil_franco/Vivado-187644-franco-ThinkPad-E15-Gen-2/incrSyn
 set_msg_config -id {Synth 8-256} -limit 10000
 set_msg_config -id {Synth 8-638} -limit 10000
 create_project -in_memory -part xc7z010clg400-1
